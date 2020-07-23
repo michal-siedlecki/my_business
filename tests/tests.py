786bddbdff96fd9b662b3bde7f73e4dc7087b655
Michal <siedlecki.michal@gmail.com>
2020-07-23 02:18:37 +0200

    test logged user views

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.models import User
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory, TestCase, Client
 
 from apps.users.views import profile
 
 
-class ProfileViewTests(TestCase):
+class NotLoggedUserViewsTests(TestCase):
+
+    def test_user_can_see_about_view(self):
+        client = Client()
+        response = client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "about")
+
+
+class LoggedUserViewsTests(TestCase):
     def setUp(self):
 
         self.factory = RequestFactory()
@@ -18,3 +28,28 @@ class ProfileViewTests(TestCase):
         response = profile(request)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_user_can_see_dashboard_view(self):
+
+        request = self.factory.get('dashboard')
+        request.user = self.user
+        response = profile(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_see_contractors_view(self):
+
+        request = self.factory.get('contractors')
+        request.user = self.user
+        response = profile(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_can_see_contractors_new_view(self):
+
+        request = self.factory.get('contractors-new')
+        request.user = self.user
+        response = profile(request)
+
+        self.assertEqual(response.status_code, 200)
+
